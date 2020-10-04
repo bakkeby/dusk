6ca11a77aa3130411ca3bde0ef1c3bea9ba4df06
bakkeby <bakkeby@gmail.com>
2020-10-04 21:06:01 +0200

    setborderpx: add support for updating bar borderpx

--- patch/setborderpx.c
+++ patch/setborderpx.c
@@ -12,26 +12,34 @@ setborderpx(const Arg *arg)
 	else
 		selmon->borderpx += arg->i;
 
+	if (enabled(BarBorder)) {
+		for (bar = selmon->bar; bar; bar = bar->next) {
+			bar->bh = bar->bh - 2 * bar->borderpx + 2 * selmon->borderpx;
+			bar->borderpx = selmon->borderpx;
+		}
+		updatebarpos(selmon);
+		for (bar = selmon->bar; bar; bar = bar->next)
+			XMoveResizeWindow(dpy, bar->win, bar->bx, bar->by, bar->bw, bar->bh);
+	}
+
 	for (c = selmon->clients; c; c = c->next)
 	{
 		if (c->bw + arg->i < 0)
-			c->bw = selmon->borderpx = 0;
+			c->bw = 0;
 		else
 			c->bw = selmon->borderpx;
 
 		if (ISFLOATING(c) || !selmon->lt[selmon->sellt]->arrange)
 		{
-			if (arg->i != 0)
+			if (arg->i != 0 && prev_borderpx + arg->i >= 0)
 				resize(c, c->x, c->y, c->w-(arg->i*2), c->h-(arg->i*2), 0);
+			else if (arg->i != 0)
+				resizeclient(c, c->x, c->y, c->w, c->h);
 			else if (prev_borderpx > borderpx)
 				resize(c, c->x, c->y, c->w + 2*(prev_borderpx - borderpx), c->h + 2*(prev_borderpx - borderpx), 0);
 			else if (prev_borderpx < borderpx)
 				resize(c, c->x, c->y, c->w - 2*(borderpx - prev_borderpx), c->h - 2*(borderpx - prev_borderpx), 0);
 		}
 	}
-
-	for (bar = selmon->bar; bar; bar = bar->next)
-		bar->borderpx = selmon->borderpx;
-
 	arrange(selmon);
 }
