cbb2fc3054ff7f2178628b30b5388e614dca72ec
bakkeby <bakkeby@gmail.com>
2020-10-04 11:45:25 +0200

    shiftview: fixed bug when scratchpad is enabled

--- patch/shiftview.c
+++ patch/shiftview.c
@@ -2,14 +2,14 @@ void
 shiftview(const Arg *arg)
 {
 	Arg shifted;
+	unsigned int seltagset = selmon->tagset[selmon->seltags] & ~SPTAGMASK;
 
 	if (arg->i > 0) // left circular shift
-		shifted.ui = (selmon->tagset[selmon->seltags] << arg->i)
-		   | (selmon->tagset[selmon->seltags] >> (NUMTAGS - arg->i));
-
+		shifted.ui = (seltagset << arg->i)
+		   | (seltagset >> (NUMTAGS - arg->i));
 	else // right circular shift
-		shifted.ui = selmon->tagset[selmon->seltags] >> (- arg->i)
-		   | selmon->tagset[selmon->seltags] << (NUMTAGS + arg->i);
+		shifted.ui = seltagset >> -arg->i
+		   | seltagset << (NUMTAGS + arg->i);
 
 	view(&shifted);
 }
