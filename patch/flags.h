fa33509bf509dfff14b40acdf7a847af6fb28ce9
bakkeby <bakkeby@gmail.com>
2020-10-24 10:48:13 +0200

    flags: enums only store integers, needed to change flags to individual unsigned long values

--- patch/flags.h
+++ patch/flags.h
@@ -5,43 +5,75 @@ static void setflag(Client *c, const unsigned long flag, const int value);
 // static void setflags(Client *c, const unsigned int flags);
 static void removeflag(Client *c, const unsigned long flag);
 
-enum {
-	Ruled = 1 << 0, // indicates whether client was subject to client rules (used internally to determine default behaviour)
-	Fixed = 1 << 1,
-	Floating = 1 << 2,
-	Urgent = 1 << 3,
-	NeverFocus = 1 << 4,
-	FullScreen = 1 << 5,
-	FakeFullScreen = 1 << 6,
-	RestoreFakeFullScreen = 1 << 7,
-	Centered = 1 << 8,
-	Permanent = 1 << 9, // client can't be killed
-	Sticky = 1 << 10, // client shows on all tags
-	// Steam = 1 << 11,
-	Terminal = 1 << 12,
-	NoSwallow = 1 << 13,
-	Locked = 1 << 14, // used by setfullscreen, prevents state change
-	Transient = 1 << 15, // whether the client has the transient or hint
-	OnlyModButtons = 1 << 16, // if enabled, allows buttons without modifiers to be used
-	NeedResize = 1 << 17,
-	AttachMaster = 1 << 18,
-	AttachAbove = 1 << 19,
-	AttachAside = 1 << 20,
-	AttachBelow = 1 << 21,
-	AttachBottom = 1 << 22,
-	SwitchTag = 1 << 23, // automatically moves you to the tag of the newly opened application
-	EnableTag = 1 << 24, // enables the tag of the newly opened application in addition to your existing enabled tags
-	RevertTag = 1 << 25, // if SwitchTag or EnableTag, closing that window reverts the view back to what it was previously
-	IgnoreCfgReq = 1 << 26, // ignore all configure requests coming from the client
-	IgnoreCfgReqPos = 1 << 27, // ignore the x,y position details of configure requests coming from the client
-	IgnoreCfgReqSize = 1 << 28, // ignore the size details of configure requests coming from the client
+static const unsigned long
+	Ruled = 0x1, // indicates whether client was subject to client rules (used internally to determine default behaviour)
+	Fixed = 0x2,
+	Floating = 0x4,
+	Urgent = 0x8,
+	NeverFocus = 0x10,
+	FullScreen = 0x20,
+	FakeFullScreen = 0x40,
+	RestoreFakeFullScreen = 0x80,
+	Centered = 0x100,
+	Permanent = 0x200, // client can't be killed
+	Sticky = 0x400, // client shows on all tags
+	Hidden = 0x800,
+	Terminal = 0x1000,
+	NoSwallow = 0x2000,
+	Locked = 0x4000, // used by setfullscreen, prevents state change
+	Transient = 0x8000, // whether the client has the transient or hint
+	OnlyModButtons = 0x10000, // if enabled, allows buttons without modifiers to be used
+	NeedResize = 0x20000,
+	AttachMaster = 0x40000,
+	AttachAbove = 0x80000,
+	AttachAside = 0x100000,
+	AttachBelow = 0x200000,
+	AttachBottom = 0x400000,
+	SwitchTag = 0x800000, // automatically moves you to the tag of the newly opened application
+	EnableTag = 0x1000000, // enables the tag of the newly opened application in addition to your existing enabled tags
+	RevertTag = 0x2000000, // if SwitchTag or EnableTag, closing that window reverts the view back to what it was previously
+	IgnoreCfgReq = 0x4000000, // ignore all configure requests coming from the client
+	IgnoreCfgReqPos = 0x8000000, // ignore the x,y position details of configure requests coming from the client
+	IgnoreCfgReqSize = 0x10000000, // ignore the size details of configure requests coming from the client
 	/* Some clients (e.g. alacritty) helpfully send configure requests with a new size or position
 	 * when they detect that they have been moved to another monitor. This can cause visual glitches
 	 * when moving (or resizing) client windows from one monitor to another. This variable is used
 	 * internally to ignore such configure requests while movemouse or resizemouse are being used. */
-	MoveResize = 1 << 29, // used internally to indicate that the client is being moved or resized
-	AlwaysOnTop = 1 << 30,
-} flags; /* flags */
+	MoveResize = 0x20000000, // used internally to indicate that the client is being moved or resized
+	AlwaysOnTop = 0x40000000,
+	TMP2147483648 = 0x80000000,
+	TMP4294967296 = 0x100000000,
+	TMP8589934592 = 0x200000000,
+	TMP17179869184 = 0x400000000,
+	TMP34359738368 = 0x800000000,
+	TMP68719476736 = 0x1000000000,
+	TMP137438953472 = 0x2000000000,
+	TMP274877906944 = 0x4000000000,
+	TMP549755813888 = 0x8000000000,
+	TMP1099511627776 = 0x10000000000,
+	TMP2199023255552 = 0x20000000000,
+	TMP4398046511104 = 0x40000000000,
+	TMP8796093022208 = 0x80000000000,
+	TMP17592186044416 = 0x100000000000,
+	TMP35184372088832 = 0x200000000000,
+	TMP70368744177664 = 0x400000000000,
+	TMP140737488355328 = 0x800000000000,
+	TMP281474976710656 = 0x1000000000000,
+	TMP562949953421312 = 0x2000000000000,
+	TMP1125899906842624 = 0x4000000000000,
+	TMP2251799813685248 = 0x8000000000000,
+	TMP4503599627370496 = 0x10000000000000,
+	TMP9007199254740992 = 0x20000000000000,
+	TMP18014398509481984 = 0x40000000000000,
+	TMP36028797018963968 = 0x80000000000000,
+	TMP72057594037927936 = 0x100000000000000,
+	TMP144115188075855872 = 0x200000000000000,
+	TMP288230376151711744 = 0x400000000000000,
+	TMP576460752303423488 = 0x800000000000000,
+	TMP1152921504606846976 = 0x1000000000000000,
+	TMP2305843009213693952 = 0x2000000000000000,
+	TMP4611686018427387904 = 0x4000000000000000,
+	TMP9223372036854775808 = 0x8000000000000000;
 
 #define ALWAYSONTOP(C) (C->flags & AlwaysOnTop)
 #define ISFLOATING(C) (C->flags & Floating)
@@ -72,6 +104,7 @@ enum {
 
 #define WASFLOATING(C) (C->prevflags & Floating)
 #define WASFAKEFULLSCREEN(C) (C->prevflags & FakeFullScreen)
+#define WASFULLSCREEN(C) (C->prevflags & FullScreen)
 
 #define SETFLOATING(C) (addflag(C, Floating))
 #define SETFULLSCREEN(C) (addflag(C, FullScreen))
