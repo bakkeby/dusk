9c642a0fedc63987b426ca52c75dddd9cbb10e85
bakkeby <bakkeby@gmail.com>
2021-11-27 23:25:28 +0100

    Proposing to only use the application icon, rather than window title, for sticky windows

--- lib/bar_wintitle_sticky.c
+++ lib/bar_wintitle_sticky.c
@@ -2,65 +2,53 @@ int
 size_wintitle_sticky(Bar *bar, BarArg *a)
 {
 	Client *c;
-	int i;
+	int w = 0;
 
-	for (c = stickyws->clients, i = 0; c; c = c->next, i++);
-	return i * 300;
+	for (c = stickyws->clients; c; c = c->next) {
+		if (!c->icon)
+			continue;
+		w += c->icw + iconspacing;
+	}
+	return w ? w - iconspacing : 0;
 }
 
 int
 draw_wintitle_sticky(Bar *bar, BarArg *a)
 {
-	if (!stickyws->clients)
-		return 0;
-	return calc_wintitle_sticky(bar, stickyws, a->x, a->w, -1, flextitledraw, NULL, a);
+	Client *c;
+	int x = a->x;
+
+	drw_setscheme(drw, scheme[bar->scheme]);
+	drw_rect(drw, a->x, a->y, a->w, a->h, 1, 1);
+	a->firstscheme = bar->scheme;
+	a->lastscheme = bar->scheme;
+
+	for (c = stickyws->clients; c; c = c->next) {
+		if (!c->icon)
+			continue;
+		drw_pic(drw, x, a->y + (a->h - c->ich) / 2, c->icw, c->ich, c->icon);
+		x += c->icw + iconspacing;
+	}
+	return x;
 }
 
 int
 click_wintitle_sticky(Bar *bar, Arg *arg, BarArg *a)
 {
+	Client *c;
 	if (!stickyws->clients)
 		return 0;
-	if (calc_wintitle_sticky(bar, stickyws, 0, a->w, a->x, flextitleclick, arg, a))
-		return ClkWinTitle;
-	return -1;
-}
+	int x = 0, w;
 
-int
-calc_wintitle_sticky(
-	Bar *bar, Workspace *ws, int offx, int tabw, int passx,
-	void(*tabfn)(Workspace *, Client *, int, int, int, int, Arg *arg, BarArg *barg),
-	Arg *arg, BarArg *a
-) {
-	Client *c;
-	int clientsnsticky = 0;
-	int groupactive = GRP_FLOAT;
-	firstpwlwintitle = 1;
-
-	for (c = ws->clients; c; c = c->next) {
-		if (SKIPTASKBAR(c))
+	for (c = stickyws->clients; c; c = c->next) {
+		if (!c->icon)
 			continue;
-		clientsnsticky++;
+		w = c->icw + iconspacing;
+		x += w;
+		if (x > a->x) {
+			arg->v =c;
+			return ClkWinTitle;
+		}
 	}
-
-	if (!clientsnsticky)
-		return 0;
-
-	/* This avoids drawing a separator on the left hand side of the wintitle section if
-	 * there is a border and the wintitle module rests at the left border. */
-	if (a->x > bar->borderpx) {
-		offx += flexwintitle_separator;
-		tabw -= flexwintitle_separator;
-	}
-
-	/* This avoids drawing a separator on the right hand side of the wintitle section if
-	 * there is a border and the wintitle module rests at the right border. */
-	if (a->x + a->w < bar->bw - bar->borderpx)
-		tabw -= flexwintitle_separator;
-
-	XSetForeground(drw->dpy, drw->gc, scheme[bar->scheme][ColBorder].pixel);
-	XFillRectangle(drw->dpy, drw->drawable, drw->gc, a->x, a->y, a->w, a->h);
-
-	flextitledrawarea(ws, ws->clients, offx, tabw, clientsnsticky, SCHEMEFOR(GRP_FLOAT), 0, 1, 1, passx, tabfn, arg, a);
-	return 1;
+	return -1;
 }
