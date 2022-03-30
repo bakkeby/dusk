bc6bff7b5d53b4e881a926748fe549db665e3e50
bakkeby <bakkeby@gmail.com>
2022-03-30 14:50:03 +0200

    setborderpx: simplifying implementation

--- lib/setborderpx.c
+++ lib/setborderpx.c
@@ -2,45 +2,26 @@ void
 setborderpx(const Arg *arg)
 {
 	Workspace *ws = selws;
+	Monitor *m = selmon;
 	Client *c;
-	Bar *bar;
-	int prev_borderpx = selmon->borderpx;
+	int prev_borderpx = m->borderpx;
+	m->borderpx = arg->i
+		? MAX(m->borderpx + arg->i, 0)
+		: m->borderpx == borderpx
+		? 0
+		: borderpx;
+	int delta = 2 * (m->borderpx - prev_borderpx);
 
-	if (arg->i == 0)
-		selmon->borderpx = borderpx;
-	else if (selmon->borderpx + arg->i < 0)
-		selmon->borderpx = 0;
-	else
-		selmon->borderpx += arg->i;
+	for (c = ws->clients; c; c = c->next) {
+		c->bw = m->borderpx;
+		if (!ISVISIBLE(c))
+			continue;
+		if (ISFULLSCREEN(c) && !ISFAKEFULLSCREEN(c))
+			continue;
+		if (!ISFLOATING(c) && ws->layout->arrange)
+			continue;
 
-	if (enabled(BarBorder)) {
-		for (bar = selmon->bar; bar; bar = bar->next) {
-			bar->bh = bar->bh - 2 * bar->borderpx + 2 * selmon->borderpx;
-			bar->borderpx = selmon->borderpx;
-		}
-		updatebarpos(selmon);
-		for (bar = selmon->bar; bar; bar = bar->next)
-			XMoveResizeWindow(dpy, bar->win, bar->bx, bar->by, bar->bw, bar->bh);
-	}
-
-	for (c = ws->clients; c; c = c->next)
-	{
-		if (c->bw + arg->i < 0)
-			c->bw = 0;
-		else
-			c->bw = selmon->borderpx;
-
-		if (ISFLOATING(c) || !ws->layout->arrange)
-		{
-			if (arg->i != 0 && prev_borderpx + arg->i >= 0)
-				resize(c, c->x, c->y, c->w-(arg->i*2), c->h-(arg->i*2), 0);
-			else if (arg->i != 0)
-				resizeclient(c, c->x, c->y, c->w, c->h);
-			else if (prev_borderpx > borderpx)
-				resize(c, c->x, c->y, c->w + 2*(prev_borderpx - borderpx), c->h + 2*(prev_borderpx - borderpx), 0);
-			else if (prev_borderpx < borderpx)
-				resize(c, c->x, c->y, c->w - 2*(borderpx - prev_borderpx), c->h - 2*(borderpx - prev_borderpx), 0);
-		}
+		resizeclient(c, c->x, c->y, c->w - delta, c->h - delta);
 	}
 	arrangews(ws);
 }
