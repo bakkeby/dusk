938c619b2d3566e844f9bbf0daa1f30b3e11588f
bakkeby <bakkeby@gmail.com>
2022-09-18 13:53:11 +0200

    swallow: do not allow terminals to swallow windows that start in fullscreen

--- dusk.c
+++ dusk.c
@@ -2220,6 +2220,7 @@ manage(Window w, XWindowAttributes *wa)
 	if (getatomprop(c, netatom[NetWMState], XA_ATOM) == netatom[NetWMFullscreen] || ISFULLSCREEN(c)) {
 		setflag(c, FullScreen, 0);
 		setfullscreen(c, 1, 0);
+		term = NULL; /* do not allow terminals to swallow windows that start in fullscreen */
 	}
 
 	updatewmhints(c);
