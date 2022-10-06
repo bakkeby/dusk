ce225f7a926c6ed84e507e0dfa9a7bfebdf8bc53
bakkeby <bakkeby@gmail.com>
2022-10-06 20:38:54 +0200

    scratchpads: edge case involving multiple scratchpads
    There was an issue such that if multiple windows are
    assigned to a scratchpad and these are brought into view
    on an empty workspace, then when hiding the windows only
    one of them would actually be hidden.
    
    This has to do with how the original showhide was removed
    in favour of having the arrange function control the
    behaviour combined with that no arrange takes place if
    there are no clients to arrange.
    
    The showwsclients calls here are redundant, but were added
    for consistency with the hide call which is the actual fix
    for this issue.

--- lib/renamed_scratchpads.c
+++ lib/renamed_scratchpads.c
@@ -103,16 +103,20 @@ togglescratch(const Arg *arg)
 			} else if (scratchvisible == numscratchpads) {
 				if (SEMISCRATCHPAD(c) && c->linked)
 					swapsemiscratchpadclients(c, c->linked);
-				else
+				else {
 					addflag(c, Invisible);
+					hide(c);
+				}
 			} else {
 				XSetWindowBorder(dpy, c->win, scheme[SchemeScratchNorm][ColBorder].pixel);
 				if (ISFLOATING(c))
 					XRaiseWindow(dpy, c->win);
 				if (SEMISCRATCHPAD(c) && c->linked)
 					swapsemiscratchpadclients(c->linked, c);
-				else
+				else {
 					removeflag(c, Invisible);
+					showwsclient(c);
+				}
 			}
 		}
 	}
@@ -131,6 +135,7 @@ togglescratch(const Arg *arg)
 		c->next = NULL;
 		attachstack(c);
 		removeflag(c, Invisible);
+		showwsclient(c);
 
 		if (ISFLOATING(c))
 			XRaiseWindow(dpy, c->win);
