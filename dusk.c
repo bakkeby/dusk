bakkeby/dusk

commit 37eae761b71c956d56169bc6444d1410136a52b2

floatpos: also store float position when client is visible and floating, to guard against obscure edge cases

lib/floatpos.c
@@ -71,6 +71,10 @@ setfloatpos(Client *c, const char *floatpos, const int auto_float)
 	if (ISVISIBLE(c) && (ISFLOATING(c) || !c->ws->layout->arrange)) {
 		getfloatpos(x, xCh, w, wCh, wx, ww, c->x, c->w, c->bw, floatposgrid_x, &c->x, &c->w);
 		getfloatpos(y, yCh, h, hCh, wy, wh, c->y, c->h, c->bw, floatposgrid_y, &c->y, &c->h);
+		c->sfx = c->x;
+		c->sfy = c->y;
+		c->sfw = c->w;
+		c->sfh = c->h;
 	} else {
 		getfloatpos(x, xCh, w, wCh, wx, ww, c->x, c->w, c->bw, floatposgrid_x, &c->sfx, &c->sfw);
 		getfloatpos(y, yCh, h, hCh, wy, wh, c->y, c->h, c->bw, floatposgrid_y, &c->sfy, &c->sfh);

commit 767dcc7acd3200aa910aac749bc69e73eb1ff4cf

37eae761 caused an issue with with undoing a toggle maximize
Refactoring to introduce a function parameter whether or not to save
the client floats depending on context.

dusk.c
@@ -629,7 +629,7 @@ applyrules(Client *c)
 			c->ws = ws ? ws : selws;
 
 			if (r->floatpos)
-				setfloatpos(c, r->floatpos, 0);
+				setfloatpos(c, r->floatpos, 0, 1);
 
 			if (REVERTWORKSPACE(c) && !c->ws->visible)
 				c->revertws = c->ws->mon->selws;
@@ -2422,11 +2422,11 @@ maximize(Client *c, int maximize_vert, int maximize_horz)
 	XRaiseWindow(dpy, c->win);
 
 	if (maximize_vert && maximize_horz)
-		setfloatpos(c, "0% 0% 100% 100%", 1);
+		setfloatpos(c, "0% 0% 100% 100%", 1, 0);
 	else if (maximize_vert)
-		setfloatpos(c, "-1x 0% -1w 100%", 1);
+		setfloatpos(c, "-1x 0% -1w 100%", 1, 0);
 	else
-		setfloatpos(c, "0% -1y 100% -1h", 1);
+		setfloatpos(c, "0% -1y 100% -1h", 1, 0);
 
 	resizeclient(c, c->x, c->y, c->w, c->h);
 	drawbar(ws->mon);

lib/floatpos.c
@@ -7,7 +7,7 @@ floatpos(const Arg *arg)
 	if (!c)
 		return;
 
-	setfloatpos(c, (char *)arg->v, 1);
+	setfloatpos(c, (char *)arg->v, 1, 1);
 	resizeclient(c, c->x, c->y, c->w, c->h);
 	savefloats(c);
 
@@ -16,7 +16,7 @@ floatpos(const Arg *arg)
 }
 
 void
-setfloatpos(Client *c, const char *floatpos, const int auto_float)
+setfloatpos(Client *c, const char *floatpos, const int auto_float, const int save_floats)
 {
 	char xCh, yCh, wCh, hCh;
 	int x, y, w, h, wx, ww, wy, wh;
@@ -71,10 +71,12 @@ setfloatpos(Client *c, const char *floatpos, const int auto_float)
 	if (ISVISIBLE(c) && (ISFLOATING(c) || !c->ws->layout->arrange)) {
 		getfloatpos(x, xCh, w, wCh, wx, ww, c->x, c->w, c->bw, floatposgrid_x, &c->x, &c->w);
 		getfloatpos(y, yCh, h, hCh, wy, wh, c->y, c->h, c->bw, floatposgrid_y, &c->y, &c->h);
-		c->sfx = c->x;
-		c->sfy = c->y;
-		c->sfw = c->w;
-		c->sfh = c->h;
+		if (save_floats) {
+			c->sfx = c->x;
+			c->sfy = c->y;
+			c->sfw = c->w;
+			c->sfh = c->h;
+		}
 	} else {
 		getfloatpos(x, xCh, w, wCh, wx, ww, c->x, c->w, c->bw, floatposgrid_x, &c->sfx, &c->sfw);
 		getfloatpos(y, yCh, h, hCh, wy, wh, c->y, c->h, c->bw, floatposgrid_y, &c->sfy, &c->sfh);

lib/floatpos.h
@@ -1,3 +1,3 @@
 static void floatpos(const Arg *arg);
-static void setfloatpos(Client *c, const char *floatpos, const int auto_float);
+static void setfloatpos(Client *c, const char *floatpos, const int auto_float, const int save_floats);
 static void getfloatpos(int pos, char pCh, int size, char sCh, int min_p, int max_s, int cp, int cs, int cbw, int defgrid, int *out_p, int *out_s);