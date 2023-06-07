c6c9562e7a8c999894102bcc386317d0b92763f9
Bakkeby <bakkeby@gmail.com>
2023-06-07 09:32:56 +0200

    Adding FocusOnClick functionality flag to default config

--- config.def.h
+++ config.def.h
@@ -81,9 +81,10 @@ static uint64_t functionality = 0
 	|BarPadding // add vertical and side padding as per vertpad and sidepad variables above
 //	|NoBorders // as per the noborder patch, show no border when only one client in tiled mode
 //	|Warp // warp cursor to currently focused window
+//	|DecorationHints // omit drawing the window border if the applications asks not to
 //	|FocusedOnTop // allows focused window to stay on top of other windows
 //	|FocusedOnTopTiled // additional toggle to allow focused tiled clients to show on top of floating windows
-//	|DecorationHints // omit drawing the window border if the applications asks not to
+//	|FocusOnClick // only allow focus change when the user clicks on windows (disables sloppy focus)
 	|FocusOnNetActive //  allow windows demanding attention to receive focus automatically
 	|AllowNoModifierButtons // allow some window operations, like move and resize, to work without having to hold down a modifier key
 	|CenterSizeHintsClients // center tiled clients subject to size hints within their tiled area
