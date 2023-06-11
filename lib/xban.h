35c34839d317f5d3e760f8ea628df7b71189eafa
Bakkeby <bakkeby@gmail.com>
2023-06-11 13:31:17 +0200

    Experimental: forward key input from selected marked client to remaining marked clients

--- lib/xban.h
+++ lib/xban.h
@@ -1,6 +1,8 @@
 #include <X11/extensions/Xfixes.h>
 #include <X11/extensions/XInput2.h>
+#include <X11/XKBlib.h>
 
+static XKeyEvent createKeyEvent(Display *dpy, Window win, Window root, int press, int keycode, int modifiers);
 static void hide_cursor(const Arg *arg);
 static void show_cursor(const Arg *arg);
 static void toggle_cursor(const Arg *arg);
@@ -8,3 +10,4 @@ static void genericevent(XEvent *e);
 
 static int xi_opcode;
 static unsigned long long last_button_press = 0;
+static unsigned int modmask = 0;
