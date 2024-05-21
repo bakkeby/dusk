293b0103185cb3e17a0e49db953df85be9d8aadc
Bakkeby <bakkeby@gmail.com>
2024-05-21 10:05:52 +0200

    bar_workspaces - renamed firsttiled to getworkspacelabelclient as the name was misleading

--- lib/bar_workspaces.c
+++ lib/bar_workspaces.c
@@ -18,7 +18,7 @@ size_workspaces(Bar *bar, BarArg *a)
 		if (!w)
 			continue;
 		if (enabled(WorkspaceLabels)) {
-			c = firsttiled(ws);
+			c = getworkspacelabelclient(ws);
 			if (c) {
 				if (c->icon && prefer_window_icons_over_workspace_labels) {
 					w += occupied_workspace_label_format_length + c->icw;
@@ -81,7 +81,7 @@ draw_workspaces(Bar *bar, BarArg *a)
 		if (ws) {
 			draw_window_icon = 0;
 			if (enabled(WorkspaceLabels)) {
-				c = firsttiled(ws);
+				c = getworkspacelabelclient(ws);
 				if (c) {
 					if (c->icon && prefer_window_icons_over_workspace_labels) {
 						draw_window_icon = 1;
@@ -165,7 +165,7 @@ click_workspaces(Bar *bar, Arg *arg, BarArg *a)
 			continue;
 
 		if (enabled(WorkspaceLabels)) {
-			c = firsttiled(ws);
+			c = getworkspacelabelclient(ws);
 			if (c) {
 				if (c->icon && prefer_window_icons_over_workspace_labels) {
 					w += occupied_workspace_label_format_length + c->icw;
@@ -219,7 +219,7 @@ hover_workspaces(Bar *bar, BarArg *a, XMotionEvent *ev)
 		if (!w)
 			continue;
 		if (enabled(WorkspaceLabels)) {
-			c = firsttiled(ws);
+			c = getworkspacelabelclient(ws);
 			if (c) {
 				if (c->icon && prefer_window_icons_over_workspace_labels) {
 					w += occupied_workspace_label_format_length + c->icw;
@@ -308,11 +308,16 @@ nextwsicon(Bar *bar, Workspace *ws, Workspace **next, char **nexticon, int *next
 	}
 }
 
+/* Used in the context of the WorkspaceLabels functionality and
+ * chooses the client that is to be used for the workspace label
+ * or icon. The first tiled client is preferred, then it falls
+ * back to the first visible client (even if that client is
+ * hidden).
+ */
 Client *
-firsttiled(Workspace *ws)
+getworkspacelabelclient(Workspace *ws)
 {
-	Client *c;
-	for (c = ws->clients; c && (ISFLOATING(c) || ISINVISIBLE(c) || HIDDEN(c)); c = c->next);
+	Client *c = nexttiled(ws->clients);
 	if (!c)
 		for (c = ws->stack; c && ISINVISIBLE(c); c = c->snext);
 	return c;
--- lib/bar_workspaces.h
+++ lib/bar_workspaces.h
@@ -5,4 +5,4 @@ static int draw_workspaces(Bar *bar, BarArg *a);
 static int click_workspaces(Bar *bar, Arg *arg, BarArg *a);
 static int hover_workspaces(Bar *bar, BarArg *a, XMotionEvent *ev);
 static void nextwsicon(Bar *bar, Workspace *ws, Workspace **next, char **nexticon, int *nextw);
-static Client *firsttiled(Workspace *ws);
+static Client *getworkspacelabelclient(Workspace *ws);
